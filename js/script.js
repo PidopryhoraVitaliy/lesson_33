7a94a8eaf91779d7a7555346552d87669c3e27e2
PidopryhoraVitaliy <vpidopryhora@gmail.com>
2020-06-23 22:57:22 +0300

    finish

--- js/script.js
+++ js/script.js
@@ -31,123 +31,125 @@ P.S. Здесь есть несколько вариантов решения з
 
 'use strict';
 
-const movieDB = {
-    movies: [
-        "Логан 2",
-        "Лига справедливости 2",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против всех"
-    ]
-};
+document.addEventListener('DOMContentLoaded', () => {
 
-document.querySelectorAll('.promo__adv').forEach(
-	elem => elem.remove()
-);
-document.querySelectorAll('.promo__genre').forEach(
-	elem => elem.textContent = 'Драма'
-);
-document.querySelectorAll('.promo__bg').forEach(
-	elem => elem.style.backgroundImage = 'url(./img/bg.jpg)'
-);
+	const movieDB = {
+		movies: [
+			"Логан 2",
+			"Лига справедливости 2",
+			"Ла-ла лэнд",
+			"Одержимость",
+			"Скотт Пилигрим против всех"
+		]
+	};
 
-const	list = document.querySelector('.promo__interactive-list'),
-		checkboxFavorite = document.querySelector('[type="checkbox"]');
-
-showMovies(list, movieDB.movies);
-
-function clearMovies(list) {
-	
-	/*document.querySelectorAll('.promo__interactive-item').forEach(
+	document.querySelectorAll('.promo__adv').forEach(
 		elem => elem.remove()
-	);*/
-	list.innerHTML = '';
-	
-};
-
-function showMovies(list, movies) {
-	
-	const maxfilmNameLength = 21;
-	
-	clearMovies(list);
-	
-	movies.sort();
-	
-	movies.forEach((item, index) => {
-		
-		//console.log(index + 1, item);
-		
-		let shortFilmName = item;
-		if (shortFilmName.length > maxfilmNameLength) {
-			shortFilmName = shortFilmName.substr(0, maxfilmNameLength) + '...';
-		};
-		
-		list.innerHTML += `
-			<li class="promo__interactive-item" data-film-name="${item}">${index + 1} ${shortFilmName}
-				<div class="delete"></div>
-			</li>
-		`;
-		
-		/*const li = document.createElement('li');
-		li.classList.add('promo__interactive-item');
-		li.textContent = `${index + 1} ${item}`;
-		list.append(li);
-		
-		const divDelete = document.createElement('div');
-		divDelete.classList.add('delete');
-		li.append(divDelete);*/
-		
-	});
-	
-	document.querySelectorAll('.delete').forEach(
-		elem => elem.addEventListener('click', (event) => {
-			
-			const	li = event.target.parentElement,
-					filmName = li.dataset.filmName,
-					filmIndex = movieDB.movies.indexOf(filmName);
-					
-			if (filmIndex == -1) {
-				console.log('error: ');
-				console.log('filmName: ' + filmName);
-				console.log('filmIndex: ' + filmIndex);
-				return;
-			};
-					
-			movieDB.movies.splice(filmIndex, 1);
-			showMovies(list, movieDB.movies);
-			
-		})
+	);
+	document.querySelectorAll('.promo__genre').forEach(
+		elem => elem.textContent = 'Драма'
+	);
+	document.querySelectorAll('.promo__bg').forEach(
+		elem => elem.style.backgroundImage = 'url(./img/bg.jpg)'
 	);
 
-};
+	const	list = document.querySelector('.promo__interactive-list'),
+			checkboxFavorite = document.querySelector('[type="checkbox"]');
 
-document.querySelector('button').addEventListener('click', (event) => {
-	
-	event.preventDefault();
-	
-	const	inputElem = document.querySelector('.adding__input'),
-			filmName = inputElem.value,
-			favorite = checkboxFavorite.checked;
-	
-	if (filmName == '') {
-		alert('Введите название фильма!');
-		return;
-	};
-	
-	if (favorite) {
-		console.log('Добавляем любимый фильм!');
-	};
-
-	movieDB.movies.push(filmName);
-	inputElem.value = '';
-	
 	showMovies(list, movieDB.movies);
-	
-	//console.log(filmName);
-	
+
+	function clearMovies(list) {
+		
+		/*document.querySelectorAll('.promo__interactive-item').forEach(
+			elem => elem.remove()
+		);*/
+		list.innerHTML = '';
+		
+	};
+
+	function showMovies(list, movies) {
+		
+		const maxfilmNameLength = 21;
+		
+		clearMovies(list);
+		
+		movies.sort();
+		
+		movies.forEach((item, index) => {
+			
+			//console.log(index + 1, item);
+			
+			let shortFilmName = item;
+			if (shortFilmName.length > maxfilmNameLength) {
+				shortFilmName = shortFilmName.substr(0, maxfilmNameLength) + '...';
+			};
+			
+			list.innerHTML += `
+				<li class="promo__interactive-item" data-film-name="${item}">${index + 1} ${shortFilmName}
+					<div class="delete"></div>
+				</li>
+			`;
+			
+			/*const li = document.createElement('li');
+			li.classList.add('promo__interactive-item');
+			li.textContent = `${index + 1} ${item}`;
+			list.append(li);
+			
+			const divDelete = document.createElement('div');
+			divDelete.classList.add('delete');
+			li.append(divDelete);*/
+			
+		});
+		
+		document.querySelectorAll('.delete').forEach(
+			elem => elem.addEventListener('click', (event) => {
+				
+				const	li = event.target.parentElement,
+						filmName = li.dataset.filmName,
+						filmIndex = movieDB.movies.indexOf(filmName);
+						
+				if (filmIndex == -1) {
+					console.log('error: ');
+					console.log('filmName: ' + filmName);
+					console.log('filmIndex: ' + filmIndex);
+					return;
+				};
+						
+				movieDB.movies.splice(filmIndex, 1);
+				showMovies(list, movieDB.movies);
+				
+			})
+		);
+
+	};
+
+	document.querySelector('button').addEventListener('click', (event) => {
+		
+		event.preventDefault();
+		
+		const	inputElem = document.querySelector('.adding__input'),
+				filmName = inputElem.value,
+				favorite = checkboxFavorite.checked;
+		
+		if (filmName == '') {
+			alert('Введите название фильма!');
+			return;
+		};
+		
+		if (favorite) {
+			console.log('Добавляем любимый фильм!');
+		};
+
+		movieDB.movies.push(filmName);
+		inputElem.value = '';
+		
+		showMovies(list, movieDB.movies);
+		
+		//console.log(filmName);
+		
+	});
+
 });
-
-
 
 
 
